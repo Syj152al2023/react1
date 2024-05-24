302fbd83db39dc3b39b9f6608e48805933370341
Syj152al2023 <syjalldyieab@gmail.com>
2024-05-24 12:06:22 +0300

    Initial commit of React+Vite project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Greeting from './comp/Greeting';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello </h1>
+      <h1>my journey with React has started!!</h1>
+      <Greeting />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/comp/Greeting.jsx
@@ -0,0 +1,14 @@
+import './Greeting.css';
+
+function Greeting() {
+    return (
+        <>
+            <p className="greeting">
+                special thanks for Karam Mostafa and all co-teachers
+            </p>
+
+        </>
+    )
+}
+
+export default Greeting;
